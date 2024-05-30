ad34b0bbe6e35a3ee2c835c5b17525ab1cbd9221
luhao661 <102971130+luhao661@users.noreply.github.com>
2024-05-30 21:22:54 +0800

    Update Cell.hpp

--- Version_1/Windows/CommonCodeBase/include/Cell.hpp
+++ Version_1/Windows/CommonCodeBase/include/Cell.hpp
@@ -25,7 +25,7 @@
 #else
 //UNIX 下标准C语言头文件
 #include <unistd.h>
-#include <arpa/inet.h> 
+#include <arpa/inet.h>
 #include <string.h>
 
 //将SOCKET类型宏定义为int类型
@@ -39,8 +39,8 @@
 //自定义
 #include "MessageHeader.hpp"//改为100字节的数据包
 #include "Timestamp.hpp"
-#include"CellTask.hpp"  
-#include "CellLog.hpp" 
+#include"CellTask.hpp"
+#include "CellLog.hpp"
 
 #include <iostream> 
 #include <stdio.h>
