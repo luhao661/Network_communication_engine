fffcb8e93f6c308bdff7960509ea108c6459ff70
luhao661 <102971130+luhao661@users.noreply.github.com>
2024-08-27 23:26:22 +0800

    Update Allocator.h

--- Contest_code_backup/server_synchronous_data_processing/TcpServer/Allocator.h
+++ Contest_code_backup/server_synchronous_data_processing/TcpServer/Allocator.h
@@ -1,16 +1,16 @@
 #ifndef _ALLOCATOR_H
 #define _ALLOCATOR_H
 
-//重载new 和new[]   delete和delete[]
+//閲嶈浇new 鍜宯ew[]   delete鍜宒elete[]
 void* operator new(size_t size);
 void operator delete(void* p);
 
 void* operator new[](size_t size);
 void operator delete[](void* p);
 
-//用新的函数去调用系统提供的malloc和free
+//鐢ㄦ柊鐨勫嚱鏁板幓璋冪敤绯荤粺鎻愪緵鐨刴alloc鍜宖ree
 void* mem_alloc(size_t size);
 void mem_free(void* p);
 
-
+ 
 #endif  /*_ALLOCATOR_H*/
